dfaef8c3f0a44e3bc945645fe1a9b0665a1c3303
Ridhan Sholeh <ridhannewbiecheaters@gmail.com>
2021-06-09 11:06:29 +0700

    Get user transactions endpoint

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -30,3 +30,48 @@ func FormatCampaignTransactions(transactions []Transaction) []CampaignTransactio
 
 	return campaignTransactionsFormatter
 }
+
+type UserTransactionFormatter struct {
+	ID        int               `json:"id"`
+	Amount    int               `json:"amount"`
+	Status    string            `json:"status"`
+	CreatedAt time.Time         `json:"created_at"`
+	Campaign  CampaignFormatter `json:"campaign"`
+}
+
+type CampaignFormatter struct {
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+}
+
+func FormatUserTransaction(transaction Transaction) UserTransactionFormatter {
+	formatter := UserTransactionFormatter{
+		ID:        transaction.ID,
+		Amount:    transaction.Amount,
+		Status:    transaction.Status,
+		CreatedAt: transaction.CreatedAt,
+	}
+
+	campaignFormatter := CampaignFormatter{
+		Name:     transaction.Campaign.Name,
+		ImageURL: "",
+	}
+	if len(transaction.Campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = transaction.Campaign.CampaignImages[0].FileName
+	}
+
+	formatter.Campaign = campaignFormatter
+
+	return formatter
+}
+
+func FormatUserTransactions(transactions []Transaction) []UserTransactionFormatter {
+	userTransactionsFormatter := []UserTransactionFormatter{}
+
+	for _, transaction := range transactions {
+		userTransactionFormatter := FormatUserTransaction(transaction)
+		userTransactionsFormatter = append(userTransactionsFormatter, userTransactionFormatter)
+	}
+
+	return userTransactionsFormatter
+}
